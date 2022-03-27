590572bedee0d8bef66025157dd4a579820d062b
Erickson Tavares <erickson@ericksontavares.com.br>
2022-03-26 23:27:52 -0300

    Inserindo administrador

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from './configs/typeorm.config';
+import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserRole } from './user-roles.enum';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class UsersService {
@@ -20,5 +29,47 @@ export class UsersService {
 
   async remove(id: string): Promise<void> {
     await this.usersRepository.delete(id);
+  }
+
+  async createAdminUser(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password != createUserDto.passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não conferem');
+    } else {
+      return this.createUser(createUserDto, UserRole.ADMIN);
+    }
+  }
+
+  async createUser(
+    createUserDto: CreateUserDto,
+    role: UserRole,
+  ): Promise<User> {
+    const { email, name, password } = createUserDto;
+
+    const user = this.usersRepository.create();
+    user.email = email;
+    user.name = name;
+    user.role = role;
+    user.status = true;
+    user.confirmationToken = crypto.randomBytes(32).toString('hex');
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+    try {
+      await user.save();
+      delete user.password;
+      delete user.salt;
+      return user;
+    } catch (error) {
+      if (error.code.toString() === '23505') {
+        throw new ConflictException('Endereço de email já está em uso');
+      } else {
+        throw new InternalServerErrorException(
+          'Erro ao salvar o usuário no banco de dados',
+        );
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
